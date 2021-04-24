d3488ed689623769aea978257a82a333ac2a60bc
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-04-24 22:07:26 +0430

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const metaData = require('./polkadot_utils/configs/metadata');
-const config = require('./polkadot_utils/configs/config');
 
 const { expect } = chai;
 const server = require('./index.js');
