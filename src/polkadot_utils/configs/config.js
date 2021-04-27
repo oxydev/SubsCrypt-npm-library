e6d6030a9b65b084077fac9646d84e8d72857743
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-04-27 23:44:13 +0430

    new deployed address is added

--- src/polkadot_utils/configs/config.js
+++ src/polkadot_utils/configs/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   node: 'ws://127.0.0.1:9944',
-  address: '5ENh2uaMEkc691BbF8637Qe71M74JZFaZNco5FYSHokSxZZK',
+  address: '5HAcCmVhCeC355nkRdipgtLgUW8wyk4wWkK5K3zm2Eqs2rrH',
 };
