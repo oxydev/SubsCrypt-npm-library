7a056020404244a823bada725182a62a5cb0e986
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-06-09 15:33:34 +0430

    small error fixed

--- src/polkadot_utils/configs/config.js
+++ src/polkadot_utils/configs/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   node: 'ws://127.0.0.1:9944',
-  address: '5C9gEAeCHEZuXMWovND6aQXGuYrVVA6GsuLGURT8GJ6qAnvT',
+  address: '5HU3ay8NUP4fUte3CvSGdQYNDqH2xLCZXB5KUdRtMtVxAd2T',
 };
--- src/polkadot_utils/configs/testConfig.js
+++ src/polkadot_utils/configs/testConfig.js
@@ -2,7 +2,7 @@ const REQUEST_TIMEOUT = 40000;
 const FAILED_STATUS = 'Failed';
 const
   SUCCESS_STATUS = 'Fetched';
-const contractAddr = '5C9gEAeCHEZuXMWovND6aQXGuYrVVA6GsuLGURT8GJ6qAnvT';
+const contractAddr = '5HU3ay8NUP4fUte3CvSGdQYNDqH2xLCZXB5KUdRtMtVxAd2T';
 const userName = 'user';
 const providerName = 'provider';
 const userAddress = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc';
@@ -15,7 +15,7 @@ const plansData = [{
   max_refund_permille_policy: '100',
   disabled: false,
 }, {
-  duration: '860,400,000',
+  duration: '864,000,000',
   price: '9,000,000',
   max_refund_permille_policy: '150',
   disabled: false,
@@ -25,7 +25,7 @@ const plansData = [{
   max_refund_permille_policy: '200',
   disabled: false,
 }];
-const plansCharacteristic = [['plan0'], ['plan1_0', 'plan1_1'], ['plan2_0', 'plan2_1', 'plan3_2']];
+const plansCharacteristic = [['plan0'], ['plan1_0', 'plan1_1'], ['plan2_0', 'plan2_1', 'plan2_2']];
 
 let userWholeData;
 
