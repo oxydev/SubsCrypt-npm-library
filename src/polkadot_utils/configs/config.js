f495147c90e5d0cf504b8a1b533ba4b17b9c1390
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-08-02 13:46:28 +0300

    update address

--- src/polkadot_utils/configs/config.js
+++ src/polkadot_utils/configs/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   node: 'ws://127.0.0.1:9944',
-  address: '5EJ2sYoczNANXmw7BH6W2v1wEdVpdvn4WqXL7bmanN9rsc9Z',
+  address: '5GSbbAhGYk7kY4i219MxUw2ud5Cn617WnEVp4FTXTjsTbngW',
 };
